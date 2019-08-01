392eb4cca112167ad75c411e178c5b73d52f26c6
Adrian Durran <adrian@intellidroid.eu>
2019-08-01 20:40:42 +0100

    moved docker-compose yml

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,10 +1,10 @@
-const path = require("path")
+const path = require('path');
 
 module.exports = {
   siteMetadata: {
     title: `Walking & Road Marching Association`,
     description: `RAF Walking and Road Marching Association`,
-    author: `@adrian`,
+    author: `@adrian`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,16 +12,28 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
+        path: path.join(__dirname, `src`, `images`)
+      }
+    },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          'article',
+          'user'
+        ],
+        queryLimit: 1000
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-transition-link`,
-    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-styled-components`
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
